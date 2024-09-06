617ed36d9518033ed09b12dd5ca28fc8156a4f9f
Mushrif Ahamed <mushrif2002@gmail.com>
2024-09-06 20:03:08 +0530

    Initial commit

--- Frontend_M/App.js
+++ Frontend_M/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ProductList from './screens/ProductList';
-import Wishlist from './screens/Wishlist';
-import ProductDetail from './screens/ProductDetail';
-
-const Stack = createStackNavigator();
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Products" component={ProductList} />
-        <Stack.Screen name="Wishlist" component={Wishlist} />
-        <Stack.Screen name="ProductDetail" component={ProductDetail} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello, welcome to Shopping Eye!</Text>
+    </View>
   );
 }
